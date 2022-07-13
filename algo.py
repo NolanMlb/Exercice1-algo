e6be9a5c21eeb610890553b39e819e6066f81cbd
NolanMlb <nolanmlb@outlook.fr>
2022-07-13 14:18:33 +0200

    Tri de la liste

--- algo.py
+++ algo.py
@@ -1,9 +1,17 @@
 import random
 
+# Création des constantes
 longueur = 8
 min = 0
 max = 20
 
+# Création de la liste avec 8 nombres aléatoires compris entre 0 et 20
 L = random.sample(range(min,max), longueur)
 
+# Tri de la liste
+LL = sorted(L)
+
+
+# Affichage
 print(L)
+print(LL)
